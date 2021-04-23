91e61d65035d7d1cf55e709bb3cd2cc83111725c
austin west <awest@demark.com>
2021-04-23 09:59:41 -0700

    add dropdown logic and various other files

--- step_definitions/input_steps.ts
+++ step_definitions/input_steps.ts
@@ -18,58 +18,60 @@ Then(
 );
 
 Then(
+  /^I select (\d+) option by index from dropdown having (.+) "(.*?)"$/,
+  async function (self: World, index: number, elementType: string, typeValue: string) {
+    await input.selectOptionFromDropdown(self, elementType, typeValue, index)
+  }
+);
+
+Then(
   /^I select "(.*?)" option by (.+) from\s*((?:multiselect)?)\sdropdown having (.+) "(.*?)"$/,
   async function (this: World, option: string, elementType: string, typeValue: string) {
     await input.selectOptionFromDropdown(this, elementType, typeValue, option)
   }
 );
 
-Then(
-  /^I select (\d+) option by index from\s*((?:multiselect)?)\sdropdown having (.+) "(.*?)"$/,
-  async function (this: World, index: number, elementType: string, typeValue: string) {
-    await input.selectOptionFromDropdown(this, elementType, typeValue, index)
-  }
-);
+
 
 Then(
   /^I select all options from multiselect dropdown having (.+) "(.*?)"$/,
   async function (this: World, elementType: string, typeValue: string) {
-    return "pass";
+    await input.selectAllOptionsFromMultiselectDropdown(this, elementType, typeValue);
   }
 );
 
 Then(
   /^I unselect all options from multiselect dropdown having (.+) "(.*?)"$/,
   async function (this: World, elementType: string, typeValue: string) {
-    return "pass";
+    await input.unselectAllOptionsFromMultiselectDropdown(this, elementType, typeValue);
   }
 );
 
 Then(
   /^I check the checkbox having (.+) "(.*?)"$/,
   async function (this: World, elementType: string, typeValue: string) {
-    return "pass";
+    await input.checkCheckbox(this, elementType, typeValue);
   }
 );
 
 Then(
   /^I uncheck the checkbox having (.+) "(.*?)"$/,
   async function (this: World, elementType: string, typeValue: string) {
-    return "pass";
+    await input.uncheckCheckbox(this, elementType, typeValue);
   }
 );
 
 Then(/^I toggle checkbox having (.+) "(.*?)"$/, async function (this: World, elementType: string, typeValue: string) {
-  return "pass";
+  await input.toggleCheckbox(this, elementType, typeValue);
 });
 
 Then(
   /^I select radio button having (.+) "(.*?)"$/,
   async function (this: World, elementType: string, typeValue: string) {
-    return "pass";
+    await input.selectRadioButton(this, elementType, typeValue);
   }
 );
 
-Then(/^I select "(.*?)" option by (.+) from radio button group having (.+) "(.*?)"$/, async function (this: World) {
-  return "pass";
+Then(/^I select "(.*?)" option by (.+) from radio button group having (.+) "(.*?)"$/, async function (this: World, elementType: string, typeValue: string, option: string) {
+  await input.selectOptionFromRadioButtonGroup(this, elementType, typeValue, option);
 });
