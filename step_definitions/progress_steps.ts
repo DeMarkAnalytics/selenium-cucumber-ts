2591f667563ed11ee5439baccf3ceea67940d5e7
austin west <awest@demark.com>
2021-04-29 06:42:03 -0700

    add proper wait

--- step_definitions/progress_steps.ts
+++ step_definitions/progress_steps.ts
@@ -4,9 +4,10 @@ import * as progress from "./functions/progress";
 
 //https://github.com/selenium-cucumber/selenium-cucumber-ruby/blob/1f21a470db8e8655746efd5466783d9e09955df1/lib/selenium-cucumber/progress_steps.rb
 
-Then(/^I (?:should\ )?wait for (\d+) sec(?:onds?)?$/, async function (this: World, seconds: string) {
-  progress.wait(this, +seconds * 1000)
+Then(/^I should wait for "(.*)" seconds$/, async function (this: World, seconds: string) {
+  await this.driver.sleep(parseInt(seconds) * 1000);
 });
+
 
 Then(
   /^I (?:should\ )?wait (\d+) seconds for element having (.+) "(.*?)" to display$/,
