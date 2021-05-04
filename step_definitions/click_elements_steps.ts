64fbca3125d16e23142f0a9e396c2e4fcc4e98f3
Bill Warner <bwarner@demark.com>
2021-05-04 12:17:01 -0700

    update

--- step_definitions/click_elements_steps.ts
+++ step_definitions/click_elements_steps.ts
@@ -6,6 +6,7 @@ import * as mouse from "./functions/clicks"
 //https://github.com/selenium-cucumber/selenium-cucumber-ruby/blob/1f21a470db8e8655746efd5466783d9e09955df1/lib/selenium-cucumber/click_elements_steps.rb
 
 When(
+  //TODO: add text parsing to this function
   /^I click on element having (id|name|class|xpath|css) "(.*?)"(?: and text "(.*?)")?$/,
   async function (this: World, elementType: string, typeValue: string, text: string) {
     validateLocater(elementType)
