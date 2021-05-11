516a201feea36919d27e79c923f1a4ebad71a8a6
Bill Warner <bwarner@demark.com>
2021-05-11 16:05:02 -0700

    bump timeout to 60 seconds

--- step_definitions/functions/clicks.ts
+++ step_definitions/functions/clicks.ts
@@ -3,29 +3,29 @@ import { elementLocator } from "./pageAssertions";
 import { waitForElementToBeLocated } from "./progress"
 
 export async function click(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 20000)
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
    await self.driver.findElement(elementLocator(elementType, typeValue)).click()
 }
 
 export async function doubleClick(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 20000)
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
    let doubleClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
    self.driver.actions().doubleClick(doubleClickElement)
  }
 
  export async function rightClick(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 20000)
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
    let doubleClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
    self.driver.actions().contextClick(doubleClickElement)
  }
 
  export async function clickForcefully(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 20000)
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
    let fouceClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
    self.driver.executeScript('arguments[0]click();', await self.driver.findElement(elementLocator(elementType, typeValue)))
  }
  
  export async function submit(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 20000)
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
    await self.driver.findElement(elementLocator(elementType, typeValue)).submit()
  }
