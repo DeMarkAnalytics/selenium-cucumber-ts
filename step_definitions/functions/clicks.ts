7f413e5aeec05c42c4c8b31dd22a274dc0a21a0c
Bill Warner <3487671+TD-4242@users.noreply.github.com>
2021-05-12 10:23:36 -0700

    Work (#2)
    * update
    
    * wait for element to load before trying to click on it
    
    * v1.0.2
    
    * bump timeout to 60 seconds
    
    * v1.0.3

--- step_definitions/functions/clicks.ts
+++ step_definitions/functions/clicks.ts
@@ -1,25 +1,31 @@
 import { World } from "../world";
 import { elementLocator } from "./pageAssertions";
+import { waitForElementToBeLocated } from "./progress"
 
 export async function click(self: World, elementType: string, typeValue: string) {
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
    await self.driver.findElement(elementLocator(elementType, typeValue)).click()
 }
 
 export async function doubleClick(self: World, elementType: string, typeValue: string) {
-    let doubleClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
-    self.driver.actions().doubleClick(doubleClickElement)
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
+   let doubleClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
+   self.driver.actions().doubleClick(doubleClickElement)
  }
 
  export async function rightClick(self: World, elementType: string, typeValue: string) {
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
    let doubleClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
    self.driver.actions().contextClick(doubleClickElement)
  }
 
  export async function clickForcefully(self: World, elementType: string, typeValue: string) {
-    let fouceClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
-    self.driver.executeScript('arguments[0]click();', await self.driver.findElement(elementLocator(elementType, typeValue)))
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
+   let fouceClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
+   self.driver.executeScript('arguments[0]click();', await self.driver.findElement(elementLocator(elementType, typeValue)))
  }
  
  export async function submit(self: World, elementType: string, typeValue: string) {
-     await self.driver.findElement(elementLocator(elementType, typeValue)).submit()
+   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
+   await self.driver.findElement(elementLocator(elementType, typeValue)).submit()
  }
