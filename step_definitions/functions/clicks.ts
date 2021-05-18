49b4dd679367e0ee35db684666ac5aa8d95f9180
dospalabras <mstorz57@gmail.com>
2021-05-18 14:18:29 -0600

    adds retry code to mouse click functions (#4)
    * adds retry code to mouse click functions
    
    * v1.0.5
    
    Co-authored-by: Bill Warner <bwarner@demark.com>

--- step_definitions/functions/clicks.ts
+++ step_definitions/functions/clicks.ts
@@ -3,29 +3,69 @@ import { elementLocator } from "./pageAssertions";
 import { waitForElementToBeLocated } from "./progress"
 
 export async function click(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
-   await self.driver.findElement(elementLocator(elementType, typeValue)).click()
+  for (var retry = 1; retry <= 10; retry++) {
+    try {
+      await waitForElementToBeLocated(self, elementType, typeValue, 60000)
+      await self.driver.findElement(elementLocator(elementType, typeValue)).click()
+      break;
+    }
+    catch (x) {
+      await self.driver.sleep(1000)
+    }
+  }
 }
 
 export async function doubleClick(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
-   let doubleClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
-   self.driver.actions().doubleClick(doubleClickElement)
- }
+  for (var retry = 1; retry <= 10; retry++) {
+    try {
+      await waitForElementToBeLocated(self, elementType, typeValue, 60000)
+      let doubleClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
+      self.driver.actions().doubleClick(doubleClickElement)
+      break;
+    }
+    catch (x) {
+      await self.driver.sleep(1000)
+    }
+  }
+}
 
- export async function rightClick(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
-   let doubleClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
-   self.driver.actions().contextClick(doubleClickElement)
- }
+export async function rightClick(self: World, elementType: string, typeValue: string) {
+  for (var retry = 1; retry <= 10; retry++) {
+    try {
+      await waitForElementToBeLocated(self, elementType, typeValue, 60000)
+      let rightClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
+      self.driver.actions().contextClick(rightClickElement)
+      break;
+    }
+    catch (x) {
+      await self.driver.sleep(1000)
+    }
+  }
+}
 
- export async function clickForcefully(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
-   let fouceClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
-   self.driver.executeScript('arguments[0]click();', await self.driver.findElement(elementLocator(elementType, typeValue)))
- }
- 
- export async function submit(self: World, elementType: string, typeValue: string) {
-   await waitForElementToBeLocated(self, elementType, typeValue, 60000)
-   await self.driver.findElement(elementLocator(elementType, typeValue)).submit()
- }
+export async function clickForcefully(self: World, elementType: string, typeValue: string) {
+  for (var retry = 1; retry <= 10; retry++) {
+    try {
+      await waitForElementToBeLocated(self, elementType, typeValue, 60000)
+      let forceClickElement = await self.driver.findElement(elementLocator(elementType, typeValue))
+      self.driver.executeScript('arguments[0]click();', forceClickElement)
+      break;
+    }
+    catch (x) {
+      await self.driver.sleep(1000)
+    }
+  }
+}
+
+export async function submit(self: World, elementType: string, typeValue: string) {
+  for (var retry = 1; retry <= 10; retry++) {
+    try {
+      await waitForElementToBeLocated(self, elementType, typeValue, 60000)
+      await self.driver.findElement(elementLocator(elementType, typeValue)).submit()
+      break;
+    }
+    catch (x) {
+      await self.driver.sleep(1000)
+    }
+  }
+}
