5ecc30f07821b6bea70e2e4b182aaef44e674f06
Bill Warner <bwarner@demark.com>
2021-07-06 12:41:51 -0700

    bugfix backspace vs enter, same thing right?

--- step_definitions/functions/keys.ts
+++ step_definitions/functions/keys.ts
@@ -19,7 +19,7 @@ export async function enter(self: World, elementType: string, typeValue: string)
   await waitForElementToBeLocated(self, elementType, typeValue, 60000);
   for (var retry = 1; retry <= 2; retry++) {
     try {
-      await self.driver.findElement(elementLocator(elementType, typeValue)).sendKeys(Key.BACK_SPACE);
+      await self.driver.findElement(elementLocator(elementType, typeValue)).sendKeys(Key.ENTER);
       break;
     } catch (x) {
       await self.driver.sleep(1000);
