93480096f4fa6849bf0317c2c3c88dc6896b47cd
Bill Warner <bwarner@demark.com>
2022-04-29 11:42:27 -0700

    updates

--- step_definitions/functions/pageAssertions.ts
+++ step_definitions/functions/pageAssertions.ts
@@ -9,23 +9,25 @@ export async function getPageTitle(self: World) {
   return await self.driver.getTitle();
 }
 
-export async function checkTitle(self: World, expected: string, negate: string) {
+export async function checkTitle(self: World, expected: string, negate?: string) {
   const pageTitle = await getPageTitle(self);
-  debugLog(`checking ${pageTitle} is equal ${expected}`);
-  if (negate === null) {
+  if (negate === "" || negate === undefined) {
+    debugLog(`checking "${pageTitle}" is equal to "${expected}"`);
     assert.strictEqual(pageTitle, expected);
   } else {
+    debugLog(`checking "${pageTitle}" is not equal to "${expected}"`);
     assert.doesNotMatch(pageTitle, new RegExp(`^${escapeRegExp(expected)}$`));
   }
 }
 
-export async function checkPartialTitle(self: World, expected: string, negate: string) {
+export async function checkPartialTitle(self: World, expected: string, negate?: string) {
   const pageTitle = await getPageTitle(self);
-  debugLog(`checking ${pageTitle} is close to ${expected}`);
 
-  if (negate === null) {
+  if (negate === "" || negate === undefined) {
+    debugLog(`checking ${pageTitle} contains ${expected}`);
     assert.match(pageTitle, new RegExp(`.*${escapeRegExp(expected)}.*`));
   } else {
+    debugLog(`checking ${pageTitle} does not contain ${expected}`);
     assert.doesNotMatch(pageTitle, new RegExp(`.*${escapeRegExp(expected)}.*`));
   }
 }
@@ -36,48 +38,48 @@ export async function getElementText(self: World, elementType: string, typeValue
   return await self.driver.findElement(elementLocator(elementType, typeValue)).getText();
 }
 
-export async function checkElementText(self: World, elementType: string, typeValue: string, expectedText: string, negate: string) {
+export async function checkElementText(self: World, elementType: string, typeValue: string, expectedText: string, negate?: string) {
   debugLog(`checking text of ${elementType} ${typeValue}, ${expectedText}`);
   await waitForElementToBeLocated(self, elementType, typeValue, 4000);
   const elementText = await getElementText(self, elementType, typeValue);
 
-  if (negate === null) {
+  if (negate === "" || negate === undefined) {
     assert.strictEqual(elementText, expectedText);
   } else {
     assert.doesNotMatch(elementText, new RegExp(`^${escapeRegExp(expectedText)}$`));
   }
 }
 
-export async function checkElementPartialText(self: World, elementType: string, typeValue: string, expectedText: string, negate: string) {
+export async function checkElementPartialText(self: World, elementType: string, typeValue: string, expectedText: string, negate?: string) {
   debugLog(`checking text of ${elementType} ${typeValue}, ${expectedText}`);
   await waitForElementToBeLocated(self, elementType, typeValue, 4000);
   const elementText = await getElementText(self, elementType, typeValue);
 
-  if (negate === null) {
+  if (negate === "" || negate === undefined) {
     assert.match(elementText, new RegExp(`.*${escapeRegExp(expectedText)}.*`));
   } else {
     assert.doesNotMatch(elementText, new RegExp(`.*${escapeRegExp(expectedText)}.*`));
   }
 }
 
-export async function checkElementAttribute(self: World, elementType: string, typeValue: string, hasType: string, hasTypeValue: string, negate: string) {
+export async function checkElementAttribute(self: World, elementType: string, typeValue: string, hasType: string, hasTypeValue: string, negate?: string) {
   debugLog(`checking if ${elementType} ${typeValue} has element ${hasType} ${hasTypeValue} `);
   await waitForElementToBeLocated(self, elementType, typeValue, 4000);
   const attributeValue = await getElementAttribute(self, elementType, typeValue, hasType);
 
-  if (negate === null) {
+  if (negate === "" || negate === undefined) {
     assert.strictEqual(attributeValue, hasTypeValue);
   } else {
     assert.strictEqual(attributeValue, null);
   }
 }
 
-export async function checkElementEnable(self: World, elementType: string, typeValue: string, status: string, negate: string) {
+export async function checkElementEnable(self: World, elementType: string, typeValue: string, status: string, negate?: string) {
   debugLog(`checking element enabled status ${elementType} ${typeValue}`);
   await waitForElementToBeLocated(self, elementType, typeValue, 4000);
   const elementStatus = await self.driver.findElement(elementLocator(elementType, typeValue)).isEnabled();
 
-  if (negate === null) {
+  if (negate === "" || negate === undefined) {
     if (status === "enabled") {
       assert(elementStatus);
     } else if (status === "disabled") {
@@ -106,10 +108,10 @@ export async function isElementDisplayed(self: World, elementType: string, typeV
   }
 }
 
-export async function checkElementPresence(self: World, elementType: string, typeValue: string, negate: string) {
+export async function checkElementPresence(self: World, elementType: string, typeValue: string, negate?: string) {
   debugLog(`checking element presence status ${elementType} ${typeValue}`);
 
-  if (negate === null) {
+  if (negate === "" || negate === undefined) {
     assert(await isElementDisplayed(self, elementType, typeValue));
   } else {
     // TODO: how to catch error here when elment doesn't exist
